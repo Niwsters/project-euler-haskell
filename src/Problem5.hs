ee8124de2cc918b2f3f4ef9d2430dcca31a48291
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2023-02-12 09:31:13 +0100

    Solve problem 5

--- src/Problem5.hs
+++ src/Problem5.hs
@@ -1,20 +1,11 @@
 module Problem5 (result) where
 
 import Primes (primeFactors)
-import Data.List (union)
+import Data.List
 
---factorMap :: [Integer] -> Map Integer [Integer] 
---factorMap numbers = go numbers (fromList [])
---  where
---    go [] m = m
---    go (x:xs) m = go xs newM
---      where
---        factors = primeFactors x
---        newM = insert x factors m
-
-factors = go [1..20] []
+factors = go (map primeFactors [1..20]) []
   where
     go [] facs = facs
-    go (x:xs) facs = go xs (ljoin facs (primeFactors x)) 
+    go (x:xs) facs = go xs ((facs \\ x) ++ x)
 
-result = factors
+result = product factors
