ab4a694d457c963dd1b480b27f1d0f9a5c7602c3
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2023-03-03 17:12:40 +0100

    Solve problem 15 even more elegantly, but still only theoretical

--- src/Problem15.hs
+++ src/Problem15.hs
@@ -1,27 +1,10 @@
 module Problem15 (result) where
 
-data Direction = D | R deriving (Enum,Show)
+f n x
+  | n == 0    = 1
+  | x == 0    = 1
+  | otherwise = (f n (x-1)) + (f (n-1) x)
 
-solve limit = move 0 0
-  where
-    canMoveRight x y = x < limit
-    canMoveDown x y = y < limit
+solve limit = f limit limit
 
-    possibleDirections x y
-      | canMoveRight' && canMoveDown' = [D,R]
-      | canMoveRight' && (not canMoveDown') = [R]
-      | (not canMoveRight') && canMoveDown' = [D]
-      | otherwise = []
-      where
-        canMoveDown' = canMoveDown x y
-        canMoveRight' = canMoveRight x y
-
-    move x y = go possible
-      where
-        possible = possibleDirections x y
-        go [D,R] = move (x+1) y + move x (y+1)
-        go [R]   = move (x+1) y
-        go [D]   = move x (y+1)
-        go []    = 1
-
-result = [solve x | x <- [0..10]]
+result = solve 15
