681d878931e08d4bd0c66d22eb7be39ee2b1e461
Sebastian Everett Eriksson <sebastian.vasser@gmail.com>
2023-03-03 23:55:18 +0100

    Solve problem 15

--- src/Problem15.hs
+++ src/Problem15.hs
@@ -1,10 +1,4 @@
 module Problem15 (result) where
 
-f n x
-  | n == 0    = 1
-  | x == 0    = 1
-  | otherwise = (f n (x-1)) + (f (n-1) x)
-
-solve limit = f limit limit
-
-result = solve 15
+row x = scanl (+) 1 (tail x)
+result = iterate row (repeat 1) !! 20 !! 20
